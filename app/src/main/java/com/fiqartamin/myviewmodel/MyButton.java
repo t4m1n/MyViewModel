1258b51cb1ceb72f9c8c18d0d23e73aef51cfeae
Zulfiqar Tamin <fiqartamin@gmail.com>
2019-08-28 15:29:04 +0700

    Initial commit

---
+++ app/src/main/java/com/fiqartamin/myviewmodel/MyButton.java
@@ -0,0 +1,46 @@
+package com.fiqartamin.myviewmodel;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatButton;
+import android.util.AttributeSet;
+
+import static android.view.Gravity.CENTER;
+
+public class MyButton extends AppCompatButton {
+    private Drawable enabledBackground, disabledBackground;
+    private int textColor;
+
+    public MyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    protected void onDraw (Canvas canvas) {
+        super.onDraw(canvas);
+        super.onDraw(canvas);
+        setBackground(isEnabled() ? enabledBackground : disabledBackground);
+        setTextColor(textColor);
+        setTextSize(12.f);
+        setGravity(CENTER);
+        setText(isEnabled() ? "Submit" : "Isi Dulu");
+    }
+
+    private void init(){
+        textColor = ContextCompat.getColor(getContext(), android.R.color.background_light);
+        enabledBackground = getResources().getDrawable(R.drawable.bg_button);
+        disabledBackground = getResources().getDrawable(R.drawable.bg_button_disable);
+    }
+}
---
+++ app/src/main/java/com/fiqartamin/myviewmodel/MyEditText.java
@@ -0,0 +1,102 @@
+package com.fiqartamin.myviewmodel;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.widget.AppCompatEditText;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class MyEditText extends AppCompatEditText{
+    Drawable mClearButtonImage;
+
+    public MyEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public MyEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyEditText(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init(){
+        mClearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_close_black_24dp, null);
+
+        setOnTouchListener(new OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if ((getCompoundDrawablesRelative()[2] != null)) {
+                    float clearButtonStart;
+                    float clearButtonEnd;
+                    boolean isClearButtonClicked = false;
+                    if (getLayoutDirection() == LAYOUT_DIRECTION_RTL) {
+                        clearButtonEnd = mClearButtonImage.getIntrinsicWidth() + getPaddingStart();
+                        if (event.getX() < clearButtonEnd) {
+                            isClearButtonClicked = true;
+                        }
+                    } else {
+                        clearButtonStart = (getWidth() - getPaddingEnd() - mClearButtonImage.getIntrinsicWidth());
+                        if (event.getX() > clearButtonStart) {
+                            isClearButtonClicked = true;
+                        }
+                    }
+                    if (isClearButtonClicked) {
+                        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_close_black_24dp, null);
+                            showClearButton();
+                            return true;
+                        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                            mClearButtonImage = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_close_black_24dp, null);
+                            if (getText() != null) {
+                                getText().clear();
+                            }
+                            hideClearButton();
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                return false;
+            }
+        });
+
+        addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (!s.toString().isEmpty()) {
+                    showClearButton();
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private void showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, mClearButtonImage, null);
+    }
+
+    private void hideClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null);
+    }
+}
